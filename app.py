eb96f0dad624d35a1729261897a090cb2c262fc1
Podcakero <github@podcake.net>
2026-01-31 11:52:18 -0500

    Update app.py to check for existence of config, generating one if it does not exist.

--- app.py
+++ app.py
@@ -1,61 +1,48 @@
 import os
+import random
 import streamlit as st
+import string
 import toml
 
-from spotipy import Spotify
-from spotipy.cache_handler import CacheHandler
-from spotipy.exceptions import SpotifyOauthError
-from spotipy.oauth2 import SpotifyOAuth
+def generate_password():
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(characters) for i in range(12))
+    return password
 
-config = toml.load("./config.toml")
+def get_config():
+    if not os.path.exists("./config.toml"):
+        generated_password = generate_password()
+        print("Generated Admin Password: " + generated_password)
 
-class StreamlitCacheHandler(CacheHandler):
-    def __init__(self):
-        self.session_id = st.session_state.get("session_id")
+        config_string = f"""
+        [general]
+        admin_password = \"{ generated_password }\"
+        dnp_file = \"FIRST-Do-Not-Play-List-2025.xlsx\"
+        remove_optional = false
+        remove_explicit = true
+        
+        [spotify]
+        client_id = \"\"
+        client_secret = \"\"
+        redirect_url = \"http://127.0.0.1:8501/spotify\"
+        scope = \"playlist-modify-public, playlist-modify-private, user-read-email\"
 
-    def get_cached_token(self):
-        return st.session_state.get("spotipy_token")
+        [youtube]
+        client_id = \"\"
+        client_secret = \"\"
+        """
 
-    def save_token_to_cache(self, token_info):
-        st.session_state["spotipy_token"] = token_info
-
-def get_auth_manager():
-    """
-    Returns a spotipy.oauth2.SpotifyOAuth object.
-    """
-    return SpotifyOAuth(
-                        client_id=os.getenv('SPOTIPY_CLIENT_ID', config['spotipy'].get('client_id', "")), 
-                        client_secret=os.getenv('SPOTIPY_CLIENT_SECRET', config['spotipy'].get('client_secret', "")), 
-                        redirect_uri=os.getenv('SPOTIPY_REDIRECT_URI', config['spotipy'].get('redirect_uri', "http://127.0.0.1:8501")),
-                        scope=os.getenv('SPOTIFY_SCOPE', config['spotipy'].get('scope', "playlist-modify-public, playlist-modify-private, user-read-email")),
-                        cache_handler=StreamlitCacheHandler())
-
-def main():
-    st.title("FRC/FTC Spotify Playlist Cleaner", anchor=None)
-
-    sp = Spotify(auth_manager=get_auth_manager())
-
-    if "spotipy_token" in st.session_state:
-        pass
+        with open("./config.toml", "w") as file:
+            config = toml.dump(toml.loads(config_string), file)
     else:
-        if st.button("Log in to Spotify"):
-            # prevents a new tab from being opened
-            st.markdown(f'<meta http-equiv="refresh" content="0; '
-                        f'url={sp.auth_manager.get_authorize_url()}"/>',
-                        unsafe_allow_html=True)
+        config = toml.load("./config.toml")
+    return config
 
-def callback():
-    code = st.query_params.get("code")
-    if code:
-        try:
-            token_info = get_auth_manager().get_access_token(code)
-        except SpotifyOauthError:
-            pass
-    del st.query_params["code"]
-    main()
+# Generate config if not existing
+get_config()
 
-#account_page = st.Page("pages/account.py", title="Spotify Account")
+admin_page = st.Page("pages/admin.py", title="Admin")
 cleaner_page = st.Page("pages/cleaner.py", title="Playlist Cleaner")
 
-pg = st.navigation([cleaner_page])
+pg = st.navigation([cleaner_page, admin_page], position="top")
 pg.run()
